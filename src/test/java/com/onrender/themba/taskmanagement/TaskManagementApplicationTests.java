f4b9fcadaf8d5cc85fc9ade53b525998957b85df
Themba G Chauke <GoodnessThembaC@gmail.com>
2023-09-08 09:57:03 +0200

    Init Task Management Application

---
+++ src/test/java/com/onrender/themba/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrender.themba.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
