b5c73ba23ae4f0ffab82d33d19e26904b25ab184
Themba G Chauke <GoodnessThembaC@gmail.com>
2023-09-08 15:46:44 +0200

    Fetch data from the server and render DOM
    Fix bugs

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,37 +1,119 @@
-document.getElementById('form-Task').addEventListener('submit', sendAddTaskRequest);
+document.getElementById('form-Task').addEventListener('submit', sendAddTaskRequest)
 
 
 function sendAddTaskRequest(e){
+  e.preventDefault()
 
   const request = new XMLHttpRequest()
 
-  let title = document.getElementById('title').value;
-  let description = document.getElementById('description').value;
+  let title = document.getElementById('title').value
+  let description = document.getElementById('description').value
 
   const data = {
     title, 
     description
   }
 
-  request.open("post", "http://127.0.0.1:8080/tasks/new", true);
+  request.open("post", "http://127.0.0.1:8080/tasks/new", true)
 
-  request.setRequestHeader("Content-Type", "application/json");
+  request.setRequestHeader("Content-Type", "application/json")
 
   request.onreadystatechange = () => {
     if(request.status = 200 && request.readyState == 4){
       const data = JSON.parse(request.response)
       console.log(data);
+      document.getElementById('form-Task').reset()
     }
   }
 
   request.onload = () =>{
-    console.log("On Loading ");
+    console.log("On Loading ")
   }
 
-  request.send(JSON.stringify(data));
+  request.send(JSON.stringify(data))
 
 }
 
+function sendFetchTaskRequest(page = 1){
+
+  const request = new XMLHttpRequest()
+
+  request.open("GET", "http://127.0.0.1:8080/tasks", true)
+
+  request.setRequestHeader("Content-Type", "application/json")
+
+
+  request.onreadystatechange = () => {
+
+    if(request.status == 200 && request.readyState == 4){
+        const data = JSON.parse(request.response)
+        console.log(data);
+        renderTask(data)
+    }
+  }
+
+  request.onload = () => {
+    console.log("Loading Data.....");
+  }
+
+  request.onerror = (error) => {
+    console.log("Server Error : ", error);
+  }
+
+  request.send()
+
+}
+
+
+sendFetchTaskRequest()
+
+function renderTask(data){
+
+  const {totalIntem , totalPage , tasks , message} = data
+
+  let tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+  let numbersTemplate = '';
+ 
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+ 
+    tasksView.innerHTML +=
+      `<div class="card mb-3">
+        <div class="card-body">
+        <div class="row">
+          <div class="col-sm-3 text-left">
+            <p>${title}</p>
+          </div>
+          <div class="col-sm-7 text-left">
+            <p>${description}</p>
+          </div>
+          <div class="col-sm-2 text-right">
+            <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">X</a>
+          </div>
+        </div>  
+       </div>
+      </div>`;
+  }
+
+  for (let i = 0; i < totalPage; i++){
+    numbersTemplate += `
+      <li class="page-item"><a class="page-link" href="#">${(i+1)}</a></li>
+    `
+  }
+
+  tasksView.innerHTML += `
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      ${numbersTemplate}
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
+  `
+
+}
 
 
  
@@ -110,4 +192,4 @@ function getTasks() {
  
 }
  
-getTasks();
+// getTasks();
