24edaa9d4a60118e60313c78cdcc0d8e45b08769
Themba G Chauke <GoodnessThembaC@gmail.com>
2023-09-08 11:45:30 +0200

    Impletement Fontend

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,113 @@
+document.getElementById('form-Task').addEventListener('submit', sendAddTaskRequest);
+
+
+function sendAddTaskRequest(e){
+
+  const request = new XMLHttpRequest()
+
+  let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+
+  const data = {
+    title, 
+    description
+  }
+
+  request.open("post", "http://127.0.0.1:8080/tasks/new", true);
+
+  request.setRequestHeader("Content-Type", "application/json");
+
+  request.onreadystatechange = () => {
+    if(request.status = 200 && request.readyState == 4){
+      const data = JSON.parse(request.response)
+      console.log(data);
+    }
+  }
+
+  request.onload = () =>{
+    console.log("On Loading ");
+  }
+
+  request.send(JSON.stringify(data));
+
+}
+
+
+
+ 
+// Save new To-Do
+function saveTask(e) {
+ 
+  let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+ 
+ 
+  let task = {
+    title,
+    description
+  };
+ 
+  if (localStorage.getItem('tasks') === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+ 
+  getTasks();
+ 
+  // Reset form-Task
+  document.getElementById('form-Task').reset();
+  e.preventDefault();
+ 
+}
+ 
+// Delete To-Do 
+function deleteTask(title) {
+ 
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].title == title) {
+      tasks.splice(i, 1);
+    }
+  }
+ 
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  getTasks();
+}
+ 
+// Show To-Do List
+function getTasks() {
+ 
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+ 
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+ 
+    tasksView.innerHTML +=
+      `<div class="card mb-3">
+        <div class="card-body">
+        <div class="row">
+          <div class="col-sm-3 text-left">
+            <p>${title}</p>
+          </div>
+          <div class="col-sm-7 text-left">
+            <p>${description}</p>
+          </div>
+          <div class="col-sm-2 text-right">
+            <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">X</a>
+          </div>
+        </div>  
+       </div>
+      </div>`;
+  }
+ 
+}
+ 
+getTasks();
